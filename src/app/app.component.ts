66e8efdcac0e249357437702b486b9efa1347c4e
Saniya Mathewkutty <S00223097@atu.ie>
2023-02-03 21:06:23 +0000

    edited app.component.ts - changed title + array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'week2';
+  title = 'Things to Do for Week Two';
+  thingstoAccomplish= [
+    'Brush up on JavaScript',
+    'Come to grips with SQL',
+    'Master programming'
+  ];
 }
